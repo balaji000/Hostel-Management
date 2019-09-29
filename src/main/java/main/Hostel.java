4966abc6081a8012b66a9c674e98066e529d523a
Rohit Sharad Varhadi <Rohit Sharad Varhadi@LAPTOP-26RGL8VJ>
2019-09-29 23:25:55 +0530

    hostel test and student test perform successfully.

--- src/main/java/main/Hostel.java
+++ src/main/java/main/Hostel.java
@@ -34,11 +34,13 @@ public class Hostel{
 		   try{
 	             BufferedReader br = new BufferedReader(new FileReader("hostel.txt"));
 	               String line = null;
-	            
+	               
 	               //write your code here !!!
 	               while ((line = br.readLine()) != null) {
 		               String[] splited = line.split("\\s+");
 		               String checkName = splited[0];
+		               if(checkName.equals(name))
+		            	   {return true;}
 		               //write your code here !!!
 //		               compare check name with name and return true if present and false if not
 	               }
@@ -47,11 +49,15 @@ public class Hostel{
 	            }catch(Exception e){
 	                System.out.println(e);
 	            }
-			return true;
+			return false;
 	   }
        public static void allotHostel(){
     	   //write your code here!!!
-    	   
+    	   try {
+			writedata();
+		} catch (IOException e) {
+			
+		}
        }
 
        public static boolean verifyStudent(int regNo){
@@ -74,7 +80,7 @@ public class Hostel{
            
        public static boolean verifyName(String name){
     	   boolean chk = true;
-    	   
+    	   chk=readData(name);
     	   //write your code here
     	   
     	   return chk;
